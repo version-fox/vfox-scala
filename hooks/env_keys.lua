15f734da877e76d6aaf1aad8762c673d35863b4b
ahai <1149956305@qq.com>
2024-04-03 10:11:07 +0800

    init plugin

--- hooks/env_keys.lua
+++ hooks/env_keys.lua
@@ -5,29 +5,16 @@
 --- @field ctx.path string SDK installation directory
 function PLUGIN:EnvKeys(ctx)
     --- this variable is same as ctx.sdkInfo['plugin-name'].path
-    local mainPath = ctx.path
-    local mainSdkInfo = ctx.main
-    local mpath = mainSdkInfo.path
-    local mversion = mainSdkInfo.version
-    local mname = mainSdkInfo.name
-    local sdkInfo = ctx.sdkInfo['sdk-name']
-    local path = sdkInfo.path
-    local version = sdkInfo.version
-    local name = sdkInfo.name
+    local path = ctx.path
     return {
         {
-            key = "JAVA_HOME",
-            value = mainPath
+            key = "SCALA_HOME",
+            value = path
         },
         {
             key = "PATH",
-            value = mainPath .. "/bin"
-        },
-        {
-            key = "PATH",
-            value = mainPath .. "/bin2"
-        },
-
+            value = path .. "/bin"
+        }
     }
 
 end
--- hooks/pre_install.lua
+++ hooks/pre_install.lua
@@ -1,3 +1,4 @@
+local util = require("util")
 --- Returns some pre-installed information, such as version number, download address, local files, etc.
 --- If checksum is provided, vfox will automatically check it for you.
 --- @param ctx table
@@ -5,35 +6,10 @@
 --- @return table Version information
 function PLUGIN:PreInstall(ctx)
     local version = ctx.version
+    local downloadUrl = util:getDownloadUrl(version)
+    print(downloadUrl)
     return {
-        --- Version number
-        version = "xxx",
-        --- remote URL or local file path [optional]
-        url = "xxx",
-        --- SHA256 checksum [optional]
-        sha256 = "xxx",
-        --- md5 checksum [optional]
-        md5 = "xxx",
-        --- sha1 checksum [optional]
-        sha1 = "xxx",
-        --- sha512 checksum [optional]
-        sha512 = "xx",
-        --- additional need files [optional]
-        addition = {
-            {
-                --- additional file name !
-                name = "xxx",
-                --- remote URL or local file path [optional]
-                url = "xxx",
-                --- SHA256 checksum [optional]
-                sha256 = "xxx",
-                --- md5 checksum [optional]
-                md5 = "xxx",
-                --- sha1 checksum [optional]
-                sha1 = "xxx",
-                --- sha512 checksum [optional]
-                sha512 = "xx",
-            }
-        }
+        version = version,
+        url = downloadUrl,
     }
 end
